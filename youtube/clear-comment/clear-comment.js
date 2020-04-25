7c8d2723a25530f18973bd6cb67d205b5e68f3e5
mkorshikov <maksim.korshikov@gmail.com>
2020-04-25 21:11:36 +0400

    Youtube clear comment

--- youtube/clear-comment/clear-comment.js
+++ youtube/clear-comment/clear-comment.js
@@ -2,7 +2,7 @@
  * Очищает комментарий при переходе к нему по ссылке
  * 
  * Добавляется на страницу с помощью вот такой ссылки
- * javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='https://raw.githubusercontent.com/mkorshikov/utilites/master/youtube/clear-comment/clear-comment.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();
+ * javascript:(function(){_my_script=document.createElement('SCRIPT');_my_script.type='text/javascript';_my_script.src='https://rawgit.com/mkorshikov/utilites/master/youtube/clear-comment/clear-comment.js';document.getElementsByTagName('head')[0].appendChild(_my_script);})();
  */
 ;(function(document) {
 	const commentHeaderEl = document.querySelector('#header-badge');
